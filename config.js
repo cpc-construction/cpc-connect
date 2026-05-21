b843451848d3a2457413ffb0f7bf19427f123ecb
darksquirrelML <107252023+darksquirrelML@users.noreply.github.com>
2026-05-21 10:39:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://ihwgejsioutzcmkaaumi.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlod2dlanNpb3V0emNta2FhdW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4OTMxMDcsImV4cCI6MjA5MTQ2OTEwN30.951EDcnMQMNzirlMxmxOLi3CmR6suhPNQNqvgvNC9rA',
+  APP_NAME: 'CPC Connect',
+  COMPANY: 'CPC Construction',
+};
